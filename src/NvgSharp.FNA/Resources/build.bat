c3b6b96c622162e69695cde4892f98ca70d58f60
Roman Shapiro <netframeworker@gmail.com>
2022-06-16 11:03:53 +0700

    Project Structure Refactoring

---
+++ src/NvgSharp.FNA/Resources/build.bat
@@ -0,0 +1,4 @@
+2mgfx Effect.fx Effect.dx11.mgfxo /Profile:DirectX_11
+2mgfx Effect.fx Effect.ogl.mgfxo /Profile:OpenGL
+
+"C:\Program Files (x86)\Windows Kits\10\bin\10.0.17763.0\x64\fxc.exe" /T fx_2_0 /Fo Effect.fxb Effect.fx
